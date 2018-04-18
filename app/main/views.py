4c05dcc4f0ce0ce96795ff171a1d6676a413f345
Peter Polle <peter.m.polle@gmail.com>
2018-04-18 16:17:22 +0300

    updated navabar view to be able to navigate to blog creation page

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,18 @@ def index():
 	title = 'Flask Base'
 	return render_template('index.html',title = title)
 
+@main.route('/admin/create_post',methods = ['GET','POST'])
+def new_post():
+	form = CreatePostForm()
+
+	if form.validate_on_submit():
+		title = form.title.data 
+		post =  form.post.data
+
+		db.session.add_all([title,post])
+		db.session.commit()
+		
+		return redirect(url_for('.index',title =  title))
+
+		title = "Create New Post"
+		return render_template('posts/create.html',form =form)
