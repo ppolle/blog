dd2c3ab41499bd736057bc191a43dc5c18afdbcb
Peter Polle <peter.m.polle@gmail.com>
2018-04-18 15:03:41 +0300

    added the Post model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
+from . import db
 from . import login_manager
 from flask_login import UserMixin
 
 
-class User(UserMixin,db.Model):
-    __tablename__ = 'users'
+class Post(db.Model):
+	__tablename__ = 'posts'
+	id = db.Column(db.Integer,primary_key = True)
+	post = db.Column(db.String(255))
+	comments = db.relationship('Comment',backref = 'role',lazy = 'dynamic')
+	
+	def __repr__(self):
+		return f'User {self.post}'
