3bd891ef92aebadc61d15054728b765416f70826
Peter Polle <peter.m.polle@gmail.com>
2018-04-19 09:58:51 +0300

    added created_at column in posts table and had it accept the time a blog post is created

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -13,6 +14,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
+simple = SimpleMDE()
 
 def create_app(config_name):
 
@@ -25,6 +27,7 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
     mail.init_app(app)
+    simple.init_app(app)
 
     # configure UploadSet
     configure_uploads(app,photos)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from . import login_manager
 from flask_login import UserMixin
+from datetime import datetime
 
 
 class Post(db.Model):
@@ -8,6 +9,7 @@ class Post(db.Model):
 	id = db.Column(db.Integer,primary_key = True)
 	title = db.Column(db.String(255))
 	post = db.Column(db.String(max))
+	created_at = db.Column(db.DateTime,default = datetime.utcnow)
 	comments = db.relationship('Comment',backref = 'role',lazy = 'dynamic')
 
 	def __repr__(self):
