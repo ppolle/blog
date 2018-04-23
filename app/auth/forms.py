fd1570894fed0130d046ee412a93df351da8c609
Peter Polle <peter.m.polle@gmail.com>
2018-04-23 13:06:42 +0300

    implemented login and logout functionality

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from wtforms import ValidationError
 from ..models import User
@@ -19,3 +19,9 @@ class RegistrationForm(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(name = data_field.data).first():
             raise ValidationError('That name is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,24 @@
 from flask import render_template,redirect,url_for
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm,LoginForm
 from . import auth
 from .. import db
+from flask_login import login_user,logout_user,login_required
 
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Blog Login"
+    return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
 def register():
@@ -17,5 +28,12 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',form = form)
+    title = "New Account"
+    return render_template('auth/register.html',form = form, title = title)
+
+#....
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
