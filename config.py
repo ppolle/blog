de14280938a85184a9d32f6144cab603a7ec0a65
Peter Polle <peter.m.polle@gmail.com>
2018-04-23 16:46:46 +0300

    update on database uri's

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	UPLOADED_PHOTOS_DEST ='app/static/photos'
-	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peter:iamBOSS12@localhost/blog'
+	
 	SECRET_KEY = os.environ.get('SECRET_KEY')
 	
 	SIMPLEMDE_JS_IIFE = True
@@ -25,6 +25,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://peter:iamBOSS12@localhost/blog'
     DEBUG = True
 
 config_options = {
